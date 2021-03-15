461aa1e368d052e99e643e41d6819f75bf1bb627
Dennis Örnberg <dennis.ornberg@gmail.com>
2021-03-15 13:13:53 +0100

    Added hashconsing example

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,22 @@ use crate::variable::Variable;
 
 fn main() {
 
-    let clause1 = Clause::new( vec![Variable::new(1, Boolean::Undef)] );
-    let clause2 = Clause::new( vec![Variable::new(2, Boolean::Undef)] );
-    let clause3 = Clause::new( vec![Variable::new(3, Boolean::Undef)] );
+    hashcons_test()
 
-    let clauses = vec![clause1, clause2, clause3];
-    print_clauses(&clauses);
+    // let clause1 = Clause::new( vec![Variable::new(1, Boolean::Undef)] );
+    // let clause2 = Clause::new( vec![Variable::new(2, Boolean::Undef)] );
+    // let clause3 = Clause::new( vec![Variable::new(3, Boolean::Undef)] );
 
-    let undecided = vec![1, 2, 3];
+    // let clauses = vec![clause1, clause2, clause3];
+    // print_clauses(&clauses);
 
-    let mut solver = Solver::new(clauses, undecided);
-    match solver.solve() {
-        true => println!("SAT"),
-        false => println!("UNSAT"),
-    }
+    // let undecided = vec![1, 2, 3];
+
+    // let mut solver = Solver::new(clauses, undecided);
+    // match solver.solve() {
+    //     true => println!("SAT"),
+    //     false => println!("UNSAT"),
+    // }
 }
 
 fn print_clauses(clauses: &Vec<Clause>) {
@@ -35,4 +37,45 @@ fn print_clauses(clauses: &Vec<Clause>) {
         println!("{:?}", clause);
     }
     println!();
+}
+
+
+use hashconsing:: { HConsed, HashConsign, HConsign };
+
+type Term = HConsed<ActualTerm>;
+#[derive(Debug, Hash, Clone, PartialEq, Eq)]
+enum ActualTerm {
+    Var(usize),
+    Lam(Term),
+    App(Term, Term)
+}
+use ActualTerm::*;
+
+fn hashcons_test() {
+    let mut factory: HConsign<ActualTerm> = HConsign::empty();
+    assert_eq! { factory.len(), 0 }
+
+    let v = factory.mk(Var(0));
+    assert_eq! { factory.len(), 1 }
+
+    let v2 = factory.mk(Var(3));
+    assert_eq! { factory.len(), 2 }
+
+    let lam = factory.mk(Lam(v2.clone()));
+    assert_eq! { factory.len(), 3 }
+
+    let v3 = factory.mk(Var(3));
+    assert_eq! { factory.len(), 3 }
+    assert_eq! { v2.uid(), v3.uid() }
+    assert_eq! { v2.get(), v3.get() }
+    assert_eq! { v2, v3 }
+
+    let lam2 = factory.mk(Lam(v3));
+    assert_eq! { factory.len(), 3 }
+    assert_eq! { lam.uid(), lam2.uid() }
+    assert_eq! { lam.get(), lam2.get() }
+    assert_eq! { lam, lam2 }
+
+    let app = factory.mk(App(lam2, v));
+    assert_eq! { factory.len(), 4 }
 }
